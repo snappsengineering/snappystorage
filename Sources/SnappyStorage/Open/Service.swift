97763dc4dbccf1041b190402b9108f98a0c0fdad
shanenoormohamed <shanen@live.ca>
2024-10-14 15:24:37 -0400

    Simplifies Saved values

--- Sources/SnappyStorage/Open/Service.swift
+++ Sources/SnappyStorage/Open/Service.swift
@@ -19,7 +19,7 @@ public protocol ServiceProtocol {
     func delete(_ object: T)  async throws
 }
 
-open class Service<T: StoredObject>: ServiceProtocol {
+open class Service<T: Storable>: ServiceProtocol {
     
     // MARK: Private Properties
     
--- Sources/SnappyStorage/Open/Storable.swift
+++ Sources/SnappyStorage/Open/Storable.swift
@@ -7,70 +7,26 @@
 
 import Foundation
 
-// MARK: - Protocol
+// MARK: - Storable Protocol
 
 public protocol Storable: Equatable, Codable {
-    var attributes: [String: Any] { get set }
+    var objectID: String { get }
+    var updatedAt: Date { get }
 }
 
-open class StoredObject: Storable  {
+// MARK: - StoredObject Struct
+
+public struct StoredObject: Storable  {
     
-    // MARK: Public properties
+    // MARK: Private properties
     
-    public var attributes: [String : Any] = [:]
+    public var objectID: String
+    public var updatedAt: Date
     
     // MARK: init
     
     init() {
         objectID = UUID().uuidString
+        updatedAt = Date()
     }
-    
-    required public init(from decoder: Decoder) throws {
-        let values = try decoder.container(keyedBy: CodingKeys.self)
-        let data = try values.decode(Data.self, forKey: .attributes)
-        attributes = try JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
-    }
-}
-
-// MARK: - Encodable
-
-extension StoredObject: Encodable {
-    enum CodingKeys: String, CodingKey {
-        case attributes
-    }
-
-    public func encode(to encoder: Encoder) throws {
-        var container = encoder.container(keyedBy: CodingKeys.self)
-        let data = try JSONSerialization.data(withJSONObject: attributes, options: [])
-        try container.encode(data, forKey: .attributes)
-    }
-}
-
-// MARK: - Equatable
-
-extension StoredObject: Equatable {
-    public static func == (lhs: StoredObject, rhs: StoredObject) -> Bool{
-        return lhs.objectID == rhs.objectID && lhs.updatedAt == rhs.updatedAt
-    }
-}
-
-// MARK: - Attributes
-
-extension StoredObject {
-    var objectID: String {
-        get { (attributes[.objectID] as? String)! }
-        set { attributes[.objectID] = newValue }
-    }
-    
-    var updatedAt: Date {
-        get { (attributes[.updatedAt] as? Date)! }
-        set { attributes[.updatedAt] = newValue }
-    }
-}
-
-// MARK: - String Extension
-
-private extension String {
-    static let objectID = "objectID"
-    static let updatedAt = "updatedAt"
 }
