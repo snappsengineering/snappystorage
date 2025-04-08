344c81ae9b77a81c2888f728541947d2d5b944ae
shanenoormohamed <shanen@live.ca>
2025-04-08 18:24:41 -0400

    Update Destination.swift

--- Sources/SnappyStorage/Destination.swift
+++ Sources/SnappyStorage/Destination.swift
@@ -17,4 +17,22 @@ public enum Destination<T: Storable> {
     static var documentsFolderName: String {
         "Documents"
     }
+    
+    var fileName: String {
+        "\(T.self).json"
+    }
+    
+    func makeURL(with fileManager: FileManager) -> URL? {
+        let result: URL?
+        switch self {
+        case .local(let directory):
+            result = fileManager.urls(for: directory, in: .userDomainMask).last
+        case .cloud:
+            result = fileManager.url(forUbiquityContainerIdentifier: nil)?
+                .appendingPathComponent(Destination.documentsFolderName)
+        case .custom(let path):
+            result = URL(string: path)?.appendingPathComponent(fileName)
+        }
+        return result?.appendingPathComponent(fileName)
+    }
 }
