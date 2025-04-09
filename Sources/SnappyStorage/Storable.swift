30927c21a05ffb65cc583e3218dd14c7bbd18d92
shanenoormohamed <shanen@live.ca>
2025-04-09 16:48:17 -0400

    Extends Storable for Equatability conformance

--- Sources/SnappyStorage/Storable.swift
+++ Sources/SnappyStorage/Storable.swift
@@ -10,3 +10,9 @@ import Foundation
 public protocol Storable: Equatable, Codable, Identifiable {
     var id: String { get }
 }
+
+extension Storable {
+    public static func == (lhs: Self, rhs: Self) -> Bool {
+        lhs.id == rhs.id
+    }
+}
