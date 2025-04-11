be2b6f703fcfe7b94d87c2ae004f4812d4a8905d
shanenoormohamed <shanen@live.ca>
2025-04-11 16:22:20 -0400

    Adds Hexgenerator resource

--- Sources/SnappyStorage/Resources/HexGenerator.swift
+++ Sources/SnappyStorage/Resources/HexGenerator.swift
@@ -8,8 +8,8 @@
 
 struct HexGenerator {
     /// Generates a random 6-digit hexadecimal string
-    static func generateHexID() -> String {
+    static func generateHexID(length: Int = 6) -> String {
         let randomValue = Int.random(in: 0...(16_777_215)) // 16_777_215 is the decimal value of FFFFFF
-        return String(format: "%06X", randomValue)
+        return String(format: "%0\(length)X", randomValue)
     }
 }
