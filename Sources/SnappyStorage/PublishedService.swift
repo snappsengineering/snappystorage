1c044013ff71f0e7db02fef77135c0b71aaac272
shanenoormohamed <shanen@live.ca>
2025-04-11 22:30:10 -0400

    Adds published service for combine operations

---
+++ Sources/SnappyStorage/PublishedService.swift
@@ -0,0 +1,34 @@
+//
+//  PublishedService.swift
+//  SnappyStorage
+//
+//  Created by Shane on 2025-04-11.
+//
+
+import Combine
+
+public class PublishedService<T: Storable>: Service<T> {
+    @Published private(set) var collectionPublisher: [T] = []
+    
+    private var cancellables = Set<AnyCancellable>()
+    
+    init() {
+        super.init()
+        self.collectionPublisher = self.collection
+    }
+    
+    open override func save(_ objectToSave: T) {
+        super.save(objectToSave)
+        self.collectionPublisher = self.collection
+    }
+    
+    open override func save(_ objectsToSave: [T]) {
+        super.save(objectsToSave)
+        self.collectionPublisher = self.collection
+    }
+    
+    open override func delete(_ objectToDelete: T) {
+        super.delete(objectToDelete)
+        self.collectionPublisher = self.collection
+    }
+}
